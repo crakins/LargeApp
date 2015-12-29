a84761254ed6c128badda86e2b35c1c7d51a7b21
Christopher Akins <rakins@gmail.com>
2015-12-29 15:00:39 -0600

    first commit

---
+++ app/mod_auth/models.py
@@ -0,0 +1,15 @@
+from app import db
+
+# Define a base model for other database tables to inherit
+class Base(db.Model):
+
+    __abstract__ = True
+    
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+
+# Define a User model
+
+    # Finish here
+    # https://www.digitalocean.com/community/tutorials/how-to-structure-large-flask-applications
